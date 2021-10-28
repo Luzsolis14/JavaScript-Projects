377dc9d0d0a207312c4f6778711c8314743f77d8
Luzsolis14 <90864462+Luzsolis14@users.noreply.github.com>
2021-10-28 14:47:03 -0700

    javascript

---
+++ Basic_JavaScrip_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+document.write('HELLO yall!!!');
+
+window.alert ( "My Name is Luz!!!!");
+
+
+var Family= "the Arezzinis", Dad= " Jeremiah ", Mom= " Hermoine ", Daughter=" Penny ", Son=" Mark ";
+
+document.write (Dad);
+document.write(Mom);
+document.write (Son);
+
+// color font here 
+
+var blues = " I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+// expression 
+document.write(5+2);
+
+
+
+
+
