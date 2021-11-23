9f293f52f873125404de8e78613330fa97f9339d
Luzsolis14 <90864462+Luzsolis14@users.noreply.github.com>
2021-11-22 16:40:03 -0800

    new

---
+++ Basic_JavaScrip_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function myFunction() { 
+    var sentence = "I am learning"; // here is the start of my sentence 
+    sentence += " a lot from this book!"; // this is added to the sentence above!!
+    document.getElementById("Concatenate").innerHTML= sentence; // document.getElement sentence 
+}
+
