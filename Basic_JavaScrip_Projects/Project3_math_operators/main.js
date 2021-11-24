6c81ab9a025abd3ecaf6e533fcd40c3ea0f514b2
Luzsolis14 <90864462+Luzsolis14@users.noreply.github.com>
2021-11-24 13:28:40 -0800

    new

--- Basic_JavaScrip_Projects/Project3_math_operators/main.js
+++ Basic_JavaScrip_Projects/Project3_math_operators/main.js
@@ -16,7 +16,7 @@ function division_function() { //dividing two numbers
 }
 function more_Math() {  //multiple operations at once 
     var simple_Math = (1+2) * 10/2 -5;
-    Document.getElementById("Math-oper").innerHTML="1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
+    document.getElementById("Math-oper").innerHTML="1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
 }
 function modulus_Operator() { //getting a remainder 
     var simple_Math = 25 % 6;
@@ -24,18 +24,19 @@ function modulus_Operator() { //getting a remainder
 }
 function negation_Operator () { // negative numbes 
     var x= 10;
-    document.getElementById("Math-neg"),innerHTML= -x;
+    document.getElementById("Math-neg").innerHTML= -x;
 }
-
-
-var X= 5.25;
-X--;
-document.write(X); //rounds down 
-
-window.alert(Math.random()*100); // this gives you a random number from 0-100
-
-var X= 5;
-X++;
-document.write(X);
-
-
+function incrementDown_Operator() { //increment down operator
+    var X= 6;
+    X--;
+    document.getElementById("Math-incD").innerHTML="When you take 1 away from 6 you get " + X;
+}
+function incrementUp_Operator() { //increment Up operator
+    var  Y = 6;
+    Y++;
+    document.getElementById("Math-incU").innerHTML="When you add 1 to 6 you get " + Y;
+}
+function random_Math() { //Picks random number 
+    var randoInt = Math.random()*100;
+    document.getElementById("Math-rando").innerHTML= "pick a random number between 0 and 100 " + randoInt; 
+}
---
+++ Basic_JavaScrip_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,4 @@
+document.write(typeof "Word"); // type of operator
+
+document.write("15" +5); // type of coercion 
+
